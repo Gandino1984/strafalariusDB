dce0675f76f578eb70c202f0f3bbe7b5b0dd2b00
Gandino1984 <andinogerman@gmail.com>
2024-05-14 15:50:13 +0200

    ssxsx

--- src/router/clienteRouter.js
+++ src/router/clienteRouter.js
@@ -5,16 +5,8 @@ import { getAll, getById, create, update, remove} from "../controllers/cliente/c
 
 import { getAll } from "../controllers/cliente/clienteViewController.js";
 
-// import dotenv from "dotenv";
-
 const router = Router();    
 
 router.get("/", getAll);
-
-// router.get("/:id", async (req,res)=>{
-//     const id = req.params.id;
-//     const result = await getById(id);
-//     res.send(result);
-// });
 
 export default router;
