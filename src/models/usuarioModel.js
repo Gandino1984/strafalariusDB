ec11af8f7eeb3bf0aab7cc0bc5c7ddb7bfac3899
onlu <anitsitska@gmail.com>
2024-05-15 10:53:36 +0200

    model usuario upadated

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -4,18 +4,26 @@ import sequelize from "../config/sequelize.js";
 
 const bandModel = sequelize.define("band",
     {
-        band_id:{
+        id_usuario:{
             type: DataTypes.INTEGER.UNSIGNED,
             allowNull:false,
             primaryKey:true,
             autoIncrement:true
         },
-        name: {
+        user_usuario: {
+            //varchar(50), not null
+            type:DataTypes.STRING(50),
+            allowNull:false
+        },
+        password_usuario:{
+            //varchar(100), not null
             type:DataTypes.STRING(100),
             allowNull:false
         },
-        creation_date:{
-            type:DataTypes.DATE
+        rol_usuario:{
+            //varchar(50), not null
+            type: DataTypes.STRING(50),
+            allowNull:false
         }
     }
 )
